7d9d5c48a89c23a31a0b6b9878a21bf09e8409f8
Rosmery-gif <00020725@uca.edu.sv>
2025-06-30 21:55:08 -0600

    Funciones de dialogos :apple:

--- src/UI.h
+++ src/UI.h
@@ -15,3 +15,37 @@ void snake();
 void mouse();
 
 void principalMenu();
+
+void selectNumber();
+
+void invalidNumber();
+
+void enemyAttacks();
+
+void youAttacks();
+
+void memorizeNumber();
+
+void numberDisappear();
+
+void writeNumber();
+
+void correct();
+
+void wrong();
+
+void weakAttck();
+
+void strongAttack();
+
+void protection();
+
+void chooseAction();
+
+void action();
+
+void sameAction();
+
+void enemyKill();
+
+void youKill();
